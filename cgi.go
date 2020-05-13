412389c2a24f9886fb9dcff6fef411d13298ec42
hsldymq <hsldymq@qq.com>
2020-05-13 18:40:46 +0800

    add cgi request meta variable names

---
+++ cgi.go
@@ -0,0 +1,21 @@
+package main
+
+const (
+	AuthType         = "AUTH_TYPE"
+	ContentLength    = "CONTENT_LENGTH"
+	ContentType      = "CONTENT_TYPE"
+	GatewayInterface = "GATEWAY_INTERFACE"
+	PathInfo         = "PATH_INFO"
+	PathTranslated   = "PATH_TRANSLATED"
+	QueryString      = "QUERY_STRING"
+	RemoteAddr       = "REMOTE_ADDR"
+	RemoteHost       = "REMOTE_HOST"
+	RemoteIdent      = "REMOTE_INDENT"
+	RemoteUser       = "REMOTE_USER"
+	RequestMethod    = "REQUEST_METHOD"
+	ScriptName       = "SCRIPT_NAME"
+	ServerName       = "SERVER_NAME"
+	ServerPort       = "SERVER_PORT"
+	ServerProtocol   = "SERVER_PROTOCOL"
+	ServerSoftware   = "SERVER_SOFTWARE"
+)
